958b22a4416fc30080842cba042a109796090d76
Li <Yadong.Li@desay-svautomotive.com>
2017-12-14 17:26:42 +0800

    try a
    Signed-off-by: Li <Yadong.Li@desay-svautomotive.com>

--- AndroidGroupTest/app/src/main/java/com/ll/fy/ftoast/MainActivity.java
+++ AndroidGroupTest/app/src/main/java/com/ll/fy/ftoast/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn=(Button)findViewById(R.id.btn_toast);
+        btn = (Button)findViewById(R.id.btn_toast);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
